927b606b532e3109ee4dc2da3cf532c47a20a971
Grishan Pradhan <grishan.pradhan@gmail.com>
2024-02-01 16:30:30 +0545

    Apply createImageBitmap() method:
    colorSpaceConversion parameter used to make the RGBA value consistent
    over different devices.

---
+++ readinUint32ArrayICC.js
@@ -0,0 +1,74 @@
+function openImage(){
+    
+    var input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/jpeg';
+
+
+    input.addEventListener('change', function(event){
+        var selectedFile=event.target.files[0];
+
+        if (selectedFile){
+            
+            console.log('Selected file:', selectedFile, '\n' , 'File type:', selectedFile.type, '\n');
+
+            var reader = new FileReader();
+
+            reader.onload = function(e){
+                const srcImg = e.target.result;
+
+                // console.log('srcImg:',srcImg);
+
+                let canvas = document.createElement('canvas');
+
+                canvas.setAttribute('id','myCanvas');
+
+                document.body.appendChild(canvas);
+                
+                let context = canvas.getContext('2d');
+
+                var img= new Image();
+
+                img.onload = async function(e){
+
+                    const theURL = e.target.src;
+
+                    console.log(img.width,img.height);
+
+                    canvasWidth = img.width;
+                    canvasHeight = img.height;
+
+                    canvas.width = canvasWidth;
+                    canvas.height = canvasHeight;
+
+                    const resp = await fetch(e.target.src);
+                    const _image_ = await resp.blob();
+                    const image_ = await createImageBitmap(_image_, {
+                        colorSpaceConversion: "none"
+                    });
+                    
+                    context.drawImage(image_, 0, 0, canvasWidth, canvasHeight);
+
+                    var imageData = context.getImageData(0,0,canvasWidth,canvasHeight);
+
+                    var pixelData = imageData.data;
+
+                    console.log(pixelData.length);
+                    console.log('The pixel data:', pixelData);
+                    const x = new Uint32Array(pixelData.buffer);
+                    console.log(x);
+
+                }
+                // img.src = "pic.jpg"
+                const objectURL = URL.createObjectURL(selectedFile);
+                console.log(objectURL);
+                img.src=objectURL;
+                console.log(img.width);
+
+            }
+            reader.readAsDataURL(selectedFile);
+        }
+    });
+    
+    input.click();
+}
