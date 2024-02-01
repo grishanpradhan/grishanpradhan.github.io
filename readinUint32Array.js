7ceddfdbdcb2ad3123120286ee161b633a80a1c9
Grishan Pradhan <grishan.pradhan@gmail.com>
2024-02-01 09:50:18 +0545

    convert the pixel which is in 8-bit R, G, B, A to 32-bit RGBA.

---
+++ readinUint32Array.js
@@ -0,0 +1,71 @@
+function openImage(){
+    
+    var input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/jpg';
+
+
+    input.addEventListener('change', function(event){
+        var selectedFile=event.target.files[0];
+
+        if (selectedFile){
+            
+            console.log('Selected file:', selectedFile, '\n' , 'File type:', selectedFile.type, '\n');
+
+            var reader = new FileReader();
+
+            
+            reader.onload = function(e){
+                const srcImg = e.target.result;
+
+                // console.log('srcImg:',srcImg);
+
+
+                let canvas = document.createElement('canvas');
+
+                canvas.setAttribute('id','myCanvas');
+
+                document.body.appendChild(canvas);
+                
+                let context = canvas.getContext('2d');
+
+                var img= new Image();
+
+                img.onload = function(){
+
+                    console.log(img.width,img.height);
+
+                    canvasWidth = img.width;
+                    canvasHeight = img.height;
+
+                    canvas.width = canvasWidth;
+                    canvas.height = canvasHeight;
+
+                    console.log(img);
+
+                    context.drawImage(img, 0, 0, canvasWidth, canvasHeight);
+
+                    var imageData = context.getImageData(0,0,canvasWidth,canvasHeight);
+
+                    var pixelData = imageData.data;
+
+
+                    console.log(pixelData.length);
+                    console.log('The pixel data:', pixelData);
+                    const x = new Uint32Array(pixelData);
+                    console.log(x);
+
+                }
+                // img.src = "pic.jpg"
+                const blob = URL.createObjectURL(selectedFile);
+                console.log(blob);
+                img.src=blob;
+                console.log(img.width);
+
+            }
+            reader.readAsDataURL(selectedFile);
+        }
+    });
+    
+    input.click();
+}
